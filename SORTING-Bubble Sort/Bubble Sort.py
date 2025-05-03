316d35ae1af57c42ab5e3b5c5c3d1c820fd0d2ec
Balaji7259 <balaji.techy22@gmail.com>
2025-05-03 11:35:31 +0530

    Initial commit

---
+++ SORTING-Bubble Sort/Bubble Sort.py
@@ -0,0 +1,10 @@
+def bubblesort(arr):
+    for iter in range(len(arr)):
+        for i in range(0,len(arr) - 1 - iter):
+            if arr[i] > arr[i+1]:
+                arr[i],arr[i+1] =arr[i+1],arr[i]
+
+    return arr
+arr = [12,13,51,26,11,14]
+print(bubblesort(arr))
+
